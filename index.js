cb12ca6db8695573cf69d02c69c05bb4965bf5a5
Aman Subhan <aman@revusys.com>
2019-11-17 02:32:50 +0500

    changed to nodejs

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const compression = require('compression')
+const rateLimit = require('express-rate-limit')
+const { body, check } = require('express-validator')
+const { pool } = require('./config')
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 10, // 10 requests,
+})
+
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: isProduction ? 'https://www.example.com' : '*',
+}
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors(origin))
+app.use(compression())
+app.use(helmet())
+app.use(limiter)
+
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addBook = (request, response) => {
+  const { author, title } = request.body
+
+  pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+    if (error) {
+      throw error
+    }
+    response.status(201).json({ status: 'success', message: 'Book added.' })
+  })
+}
+
+app
+  .route('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
